24843d460236533a6bac4569f3d519d10370ce1d
Phystro <phystroq@gmail.com>
2024-06-01 10:28:44 +0300

    some changes

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -11,7 +11,7 @@ local indent = 4
 opt.mouse = "a"
 
 opt.autowrite = true -- Enable auto write
-opt.list = true -- Show some invisible whitespace characters (tabs...
+opt.list = true      -- Show some invisible whitespace characters (tabs...
 -- vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 -- opt.listchars:append("space:·")
 -- opt.listchars:append("nbsp:⎵")
@@ -30,34 +30,34 @@ opt.completeopt = { "menu", "menuone", "preview", "noselect" }
 opt.pumheight = 10
 
 -- Tab
-opt.tabstop = indent -- number of visual spaces per TAB
+opt.tabstop = indent     -- number of visual spaces per TAB
 opt.softtabstop = indent -- number of spacesin tab when editing
-opt.expandtab = true -- tabs are spaces, mainly for python
+opt.expandtab = true     -- tabs are spaces, mainly for python
 
 -- INDENT
-opt.smartindent = true -- smart with indent
-opt.breakindent = true -- enable break indent
-opt.autoindent = true -- indent new line same amount of previous line
+opt.smartindent = true  -- smart with indent
+opt.breakindent = true  -- enable break indent
+opt.autoindent = true   -- indent new line same amount of previous line
 opt.shiftwidth = indent -- width of a TAB autoindent
 
 -- UI config
-opt.number = true -- show absolute number
+opt.number = true         -- show absolute number
 opt.relativenumber = true -- add numbers to each line on left side
-opt.wrap = true -- wrap lines
-opt.scrolloff = 12 -- min number of lines around cursor (n above, n below) Minimal number of screen lines to keep above and below the cursor.
-opt.sidescrolloff = 12 -- scroll page when cursror is # spaces from left/right
-opt.cursorline = true -- highlight cursor line underneath the cursor horizontally
-opt.splitbelow = true -- open new vertical split botton
-opt.splitright = true -- open new horizontal splits right
-opt.termguicolors = true -- enable 24-bit RGB color in the TUI
-opt.background = "dark" -- colorschemes that can be light or dark will be made dart
-opt.showmode = true -- showing the mode ``-- INSERT --``
-opt.showtabline = 0 -- always show tabs
-opt.showcmd = true -- show last command
-opt.cmdheight = 1 -- more space in the neovim command line for displaying messages
-opt.signcolumn = "yes" -- always draw the sign column
-opt.cursorcolumn = false -- vertical column on through the cursor
--- opt.colorcolumn = "88" -- colorized the 88th column
+opt.wrap = true           -- wrap lines
+opt.scrolloff = 12        -- min number of lines around cursor (n above, n below) Minimal number of screen lines to keep above and below the cursor.
+opt.sidescrolloff = 12    -- scroll page when cursror is # spaces from left/right
+opt.cursorline = true     -- highlight cursor line underneath the cursor horizontally
+opt.splitbelow = true     -- open new vertical split botton
+opt.splitright = true     -- open new horizontal splits right
+opt.termguicolors = true  -- enable 24-bit RGB color in the TUI
+opt.background = "dark"   -- colorschemes that can be light or dark will be made dart
+opt.showmode = true       -- showing the mode ``-- INSERT --``
+opt.showtabline = 0       -- always show tabs
+opt.showcmd = true        -- show last command
+opt.cmdheight = 1         -- more space in the neovim command line for displaying messages
+opt.signcolumn = "yes"    -- always draw the sign column
+opt.cursorcolumn = false  -- vertical column on through the cursor
+opt.colorcolumn = "88"    -- colorized the 88th column
 
 opt.fillchars = { vert = "|" }
 -- opt.shortmess:append { 'c' }
@@ -65,30 +65,30 @@ opt.laststatus = 3 -- always show statusline
 opt.backspace = { "start", "eol", "indent" }
 
 -- Searching
-opt.incsearch = true -- search as characters are entered
-opt.hlsearch = true -- whether to highlight matches
-opt.ignorecase = true -- ignore case in searches by default
-opt.smartcase = true -- whether to make case sensitive if an uppercase is entered
-opt.inccommand = "split" -- preview substitutions live, as you type!
-opt.syntax = "ON" -- syntax highlighting
+opt.incsearch = true         -- search as characters are entered
+opt.hlsearch = true          -- whether to highlight matches
+opt.ignorecase = true        -- ignore case in searches by default
+opt.smartcase = true         -- whether to make case sensitive if an uppercase is entered
+opt.inccommand = "split"     -- preview substitutions live, as you type!
+opt.syntax = "ON"            -- syntax highlighting
 opt.grepprg = "rg --vimgrep" -- ?
 
-opt.updatetime = 300 -- update time for the swap file and for the cursor Hold event
+opt.updatetime = 300         -- update time for the swap file and for the cursor Hold event
 opt.timeoutlen = 500
 opt.ttimeoutlen = 10
 
 -- BACKUP
-opt.backup = false -- no backup of current file is made
-opt.swapfile = false -- default backup behaviour
-opt.undofile = true -- undo file behaviour/save undo history
+opt.backup = false                                 -- no backup of current file is made
+opt.swapfile = false                               -- default backup behaviour
+opt.undofile = true                                -- undo file behaviour/save undo history
 opt.undodir = os.getenv("HOME") .. "/.vim/undodir" -- use the directory of undotree plugin for managing the history
 
 -- CONTEXTUAL
-opt.title = true --set title of window automatically, useful for tabs plugin
-opt.path:append({ "**" }) -- search (gf or :find) files down into subfolders
+opt.title = true                --set title of window automatically, useful for tabs plugin
+opt.path:append({ "**" })       -- search (gf or :find) files down into subfolders
 opt.titlestring = "Neovim - %t" -- title string
 opt.encoding = "utf-8"
-opt.fileencoding = "utf-8" -- encoding set to utf-8
+opt.fileencoding = "utf-8"      -- encoding set to utf-8
 
 -- CODE FOLDING
 opt.foldcolumn = "1" -- '0' is not bad
--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -1,13 +1,13 @@
 return {
-    "folke/trouble.nvim",
-    event = { 'BufReadPre', 'BufNewFile' },
-    dependencies = {
-    "nvim-tree/nvim-web-devicons",
-},
-opts = function()
-    return require("plugins_options.trouble_options")
-end,
-config = function(_, opts)
-    require("trouble").setup(opts)
-end,
+	"folke/trouble.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+	},
+	opts = function()
+		return require("plugins_options.trouble_options")
+	end,
+	config = function(_, opts)
+		require("trouble").setup(opts)
+	end,
 }
