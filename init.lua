9124f12f32cae6cd6d0f412a4adbb6968eff6be5
PhyTensor <phystroq@gmail.com>
2025-03-31 02:33:37 +0300

    stably functioning, ready to use config

--- init.lua
+++ init.lua
@@ -1,2 +1,20 @@
-require("config.lazy")
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+-- Set <space> as the leader key
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
 
+-- Set to true if you have a Nerd Font installed and selected in the terminal
+vim.g.have_nerd_font = true
+
+-- Enable LazyVim auto format
+vim.g.autoformat = true
+
+require 'config.options'
+require 'config.keymaps'
+require 'config.autocmds'
+
+require 'config.lazy'
+
+vim.cmd [[ colorscheme tokyonight-night ]]
--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -17,20 +17,6 @@ end
 
 vim.opt.rtp:prepend(lazypath)
 
--- Make sure to setup `mapleader` and `maplocalleader` before
--- loading lazy.nvim so that mappings are correct.
--- This is also a good place to setup other settings (vim.opt)
--- Set <space> as the leader key
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
-
--- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = true
-
-require 'config.options'
-require 'config.keymaps'
-require 'config.autocmds'
-
 -- Setup lazy.nvim
 require('lazy').setup {
     spec = {
@@ -39,7 +25,50 @@ require('lazy').setup {
     },
     -- Configure any other settings here. See the documentation for more details.
     -- colorscheme that will be used when installing plugins.
-    install = { colorscheme = { 'habamax' } },
+    install = {
+        colorscheme = {
+            -- 'kanagawa',
+            -- 'habamax',
+        },
+    },
     -- automatically check for plugin updates
     checker = { enabled = false },
+    change_detection = { notify = true },
+    performance = {
+        cache = {
+            enabled = true,
+        },
+        reset_packpath = true, -- reset the package path to improve startup time
+        rtp = {
+            disabled_plugins = {
+                '2html_plugin',
+                'tohtml',
+                'getscript',
+                'getscriptPlugin',
+                'gzip',
+                'logipat',
+                'netrw',
+                'netrwPlugin',
+                'netrwSettings',
+                'netrwFileHandlers',
+                'matchit',
+                'tar',
+                'tarPlugin',
+                'rrhelper',
+                'spellfile_plugin',
+                'vimball',
+                'vimballPlugin',
+                'zip',
+                'zipPlugin',
+                'tutor',
+                'rplugin',
+                'syntax',
+                'synmenu',
+                'optwin',
+                'compiler',
+                'bugreport',
+                'ftplugin',
+            },
+        },
+    },
 }
