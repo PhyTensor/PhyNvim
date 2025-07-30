0a985f58452576b8d3d951a931c349ed60be5fa6
PhyTensor <phystroq@gmail.com>
2025-07-30 23:19:40 +0300

    fix(config): treesj keymaps

--- lua/plugins/treesj.lua
+++ lua/plugins/treesj.lua
@@ -3,11 +3,29 @@ return {
 	enabled = true,
 	dependencies = { "nvim-treesitter/nvim-treesitter" },
 	keys = {
-		"<leader>m",
-		"<leader>j",
-		"<leader>s",
+		{
+			"<leader>m",
+			mode = { "n", "v" },
+			function()
+				require("treesj").toggle({ split = { recursive = true } })
+			end,
+			desc = "Toggle split or join code block",
+		},
+		{
+			"<leader>j",
+			mode = { "n", "v" },
+			function()
+				require("treesj").join()
+			end,
+			desc = "[J]oin code block",
+		},
+		{
+			"<leader>ss",
+			mode = { "n", "v" },
+			function()
+				require("treesj").split()
+			end,
+			desc = "[S]plit code block",
+		},
 	},
-	config = function()
-		require("treesj").setup({})
-	end,
 }
